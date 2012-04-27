548b3258099db5095e352b4d931ee0dc44233b1d
Klas Lindfors <klas@yubico.com>
2012-04-27 14:13:44 +0200

    simple test application

---
+++ YubicoDotNetTest/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace YubicoDotNetTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.otpInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clientInput = new System.Windows.Forms.TextBox();
+            this.keyInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // otpInput
+            // 
+            this.otpInput.Location = new System.Drawing.Point(13, 27);
+            this.otpInput.Name = "otpInput";
+            this.otpInput.Size = new System.Drawing.Size(100, 20);
+            this.otpInput.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.submit);
+            // 
+            // clientInput
+            // 
+            this.clientInput.Location = new System.Drawing.Point(13, 54);
+            this.clientInput.Name = "clientInput";
+            this.clientInput.Size = new System.Drawing.Size(100, 20);
+            this.clientInput.TabIndex = 2;
+            // 
+            // keyInput
+            // 
+            this.keyInput.Location = new System.Drawing.Point(13, 81);
+            this.keyInput.Name = "keyInput";
+            this.keyInput.Size = new System.Drawing.Size(100, 20);
+            this.keyInput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OTP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ClientId";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "apiKey";
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Location = new System.Drawing.Point(13, 157);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(0, 13);
+            this.output.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyInput);
+            this.Controls.Add(this.clientInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.otpInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox otpInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox clientInput;
+        private System.Windows.Forms.TextBox keyInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ YubicoDotNetTest/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using YubicoDotNetClient;
+
+namespace YubicoDotNetTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit(object sender, EventArgs e)
+        {
+            String otp = otpInput.Text;
+            String clientId = clientInput.Text;
+            String apiKey = keyInput.Text;
+
+            YubicoClient client = new YubicoClient(clientId);
+            if (!apiKey.Equals(""))
+            {
+                client.setApiKey(apiKey);
+            }
+            YubicoResponse response = client.verify(otp);
+            output.Text = response.getStatus().ToString();
+        }
+    }
+}

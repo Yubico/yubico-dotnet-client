14cf19d3ea187b59108d74d7d758c4f0f30e8d90
Klas Lindfors <klas@yubico.com>
2012-05-01 21:24:45 +0200

    larger box

--- YubicoDotNetTest/Form1.Designer.cs
+++ YubicoDotNetTest/Form1.Designer.cs
@@ -144,7 +144,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 311);
             this.Controls.Add(this.nonce);
             this.Controls.Add(this.nonceInput);
             this.Controls.Add(this.syncLabel);
